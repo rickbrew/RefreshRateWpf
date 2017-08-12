97db5dd89d47b03f1a5b6d7df70da9347528a363
Rick Brewster <rickbrew@users.noreply.github.com>
2017-08-12 16:47:46 -0700

    Initial check-in

---
+++ RefreshRateWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RefreshRateWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.timer = new DispatcherTimer();
+            this.timer.Interval = TimeSpan.FromMilliseconds(250);
+            this.timer.Tick += OnTimerTick;
+            this.timer.Start();
+        }
+
+        private unsafe void OnTimerTick(object sender, object e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            IntPtr hwnd = helper.Handle;
+
+            IntPtr hmonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            MONITORINFOEXW monitorInfo = new MONITORINFOEXW();
+            monitorInfo.cbSize = (uint)Marshal.SizeOf<MONITORINFOEXW>();
+
+            bool bResult = GetMonitorInfoW(hmonitor, ref monitorInfo);
+
+            DEVMODEW devMode = new DEVMODEW();
+            devMode.dmSize = (ushort)Marshal.SizeOf<DEVMODEW>();
+
+            bResult = EnumDisplaySettingsW(monitorInfo.szDevice, ENUM_CURRENT_SETTINGS, out devMode);
+
+            this.textBlock.Text = string.Format("{0} x {1} @ {2}hz", devMode.dmPelsWidth, devMode.dmPelsHeight, devMode.dmDisplayFrequency);
+        }
+
+        // MonitorFromWindow
+        private const uint MONITOR_DEFAULTTONEAREST = 2;
+
+        [DllImport("user32.dll", SetLastError = false)]
+        private static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
+        // RECT
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        // MONITORINFOEX
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private unsafe struct MONITORINFOEXW
+        {
+            public uint cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public uint dwFlags;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string szDevice;
+        }
+
+        // GetMonitorInfo
+        [DllImport("user32.dll", SetLastError = false, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetMonitorInfoW(
+            IntPtr hMonitor,
+            ref MONITORINFOEXW lpmi);
+
+        // EnumDisplaySettings
+        private const uint ENUM_CURRENT_SETTINGS = unchecked((uint)-1);
+
+        [DllImport("user32.dll", SetLastError = false, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumDisplaySettingsW(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpszDeviceName,
+            uint iModeNum,
+            out DEVMODEW lpDevMode);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct DEVMODEW
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmDeviceName;
+
+            public ushort dmSpecVersion;
+            public ushort dmDriverVersion;
+            public ushort dmSize;
+            public ushort dmDriverExtra;
+            public uint dmFields;
+
+            /*public short dmOrientation;
+            public short dmPaperSize;
+            public short dmPaperLength;
+            public short dmPaperWidth;
+            public short dmScale;
+            public short dmCopies;
+            public short dmDefaultSource;
+            public short dmPrintQuality;*/
+            // These next 4 int fields are a union with the above 8 shorts, but we don't need them right now
+            public int dmPositionX;
+            public int dmPositionY;
+            public uint dmDisplayOrientation;
+            public uint dmDisplayFixedOutput;
+
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmFormName;
+
+            public short dmLogPixels;
+            public uint dmBitsPerPel;
+            public uint dmPelsWidth;
+            public uint dmPelsHeight;
+
+            public uint dmNupOrDisplayFlags;
+            public uint dmDisplayFrequency;
+
+            public uint dmICMMethod;
+            public uint dmICMIntent;
+            public uint dmMediaType;
+            public uint dmDitherType;
+            public uint dmReserved1;
+            public uint dmReserved2;
+            public uint dmPanningWidth;
+            public uint dmPanningHeight;
+        }
+
+    }
+}
